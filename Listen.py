9f69c3f05005762d25d025b70da8ccac82e4132a
Heda Fatima <fatimaelamri789@gmail.com>
2024-05-21 15:51:36 +0200

    random key

--- Listen.py
+++ Listen.py
@@ -1,10 +1,32 @@
 import time
 import keyboard
+import random
 
-time.sleep(3)
-while True:
+def write(phrase):
     keyboard.write('Hello Word!')
     time.sleep(1)
     keyboard.press('enter')
     time.sleep(1)
     keyboard.release('enter')
+
+def random_keys():
+    keys = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 
+            's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 
+            'enter', 'space', 'backspace', 'tab', 'esc', 'up', 'down', 'left', 'right']
+    
+    random_key = random.choice(keys)
+    time.sleep(1)
+    keyboard.press(random_key)
+    time.sleep(1)
+    keyboard.release(random_key)
+
+# time.sleep(3)
+# while True:
+    # write('Hello Word!')
+    # keyboard.write('Hello Word!')
+    # time.sleep(1)
+    # keyboard.press('enter')
+    # time.sleep(1)
+    # keyboard.release('enter')
+    
+random_keys()
